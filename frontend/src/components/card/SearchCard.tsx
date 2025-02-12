63ecd98d41dafbf511f7cc702fff40aa5b188e7d
Simo672k <mohammed.hakmiii555@gmail.com>
2025-02-12 23:27:10 +0100

    feat: using react-hook-form to manage and validate problems card

--- frontend/src/components/card/SearchCard.tsx
+++ frontend/src/components/card/SearchCard.tsx
@@ -1,7 +1,10 @@
+import SearchForm from "../form/SearchForm";
+
 const SearchCard = () => {
   return (
     <div className="bg-white rounded-md border shadow py-4 px-6">
-      <h3 className="text-lg font-medium">Find Problems & Pain Points</h3>
+      <h3 className="text-lg font-medium mb-4">Find Problems & Pain Points</h3>
+      <SearchForm />
     </div>
   );
 };
--- frontend/src/components/form/SearchForm.tsx
+++ frontend/src/components/form/SearchForm.tsx
@@ -1,4 +1,71 @@
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface FormData {
+  subreddit: string;
+  timeframe: string;
+}
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isValid },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col mb-4">
+        <label htmlFor="subbreddit" className="text-gray-600 mb-1">
+          <span className="text-red-500 font-bold">*</span> Subreddit
+        </label>
+        <input
+          id="subbreddit"
+          className="w-full px-3 py-1.5 border border-gray-300 rounded shadow-sm focus:outline-none  focus:border-blue-300 focus:ring-1 focus:ring-blue-300 text-gray-900 placeholder-gray-400"
+          type="text"
+          {...register("subreddit", { required: "Subreddit name is required" })}
+          onChange={(e) => setValue("subreddit", e.target.value.trim())}
+          placeholder="Enter a subreddit without /r"
+        />
+        {errors.subreddit && (
+          <p className="text-red-500 text-sm mt-1">
+            {errors.subreddit.message}
+          </p>
+        )}
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="timeframe" className="text-gray-600 mb-1">
+          <span className="text-red-500 font-bold">*</span> Time Frame
+        </label>
+
+        <select
+          {...register("timeframe")}
+          id="timeframe"
+          className="w-full px-3 py-1.5 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 text-gray-900"
+        >
+          <option value="week">Last Week</option>
+          <option value="month">Last Month</option>
+          <option value="year">Last Year</option>
+        </select>
+
+        {errors.timeframe && (
+          <p className="text-red-500">{errors.timeframe.message}</p>
+        )}
+      </div>
+
+      <button
+        type="submit"
+        className="w-full mt-4 py-2 px-4 rounded text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none transition-colors"
+      >
+        Find Problems
+      </button>
+    </form>
+  );
 };
 export default SearchForm;
